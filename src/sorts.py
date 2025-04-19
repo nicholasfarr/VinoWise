243560f4662ac1025fd4406e5c72ec84d5f89277
LandonB5 <landon.b5@icloud.com>
2025-04-19 17:07:53 -0400

    basic merge and quick sort

---
+++ src/sorts.py
@@ -0,0 +1,51 @@
+def merge_sort(items):
+    if len(items) <= 1:
+        return items
+    mid = len(items) // 2
+    left = merge_sort(items[:mid])
+    right = merge_sort(items[mid:])
+    merged = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+       
+        if left[i][1] >= right[j][1]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+    return merged
+
+def merge_sort_top5(d):
+
+    items = list(d.items())             
+    sorted_items = merge_sort(items)
+    return [key for key, _ in sorted_items[:5]]
+
+
+def quick_sort(items, low=0, high=None):
+    if high is None:
+        high = len(items) - 1
+    def partition(lo, hi):
+        pivot = items[hi][1]
+        i = lo - 1
+        for j in range(lo, hi):
+            if items[j][1] >= pivot:  
+                i += 1
+                items[i], items[j] = items[j], items[i]
+        items[i+1], items[hi] = items[hi], items[i+1]
+        return i+1
+
+    if low < high:
+        p = partition(low, high)
+        quick_sort(items, low, p - 1)
+        quick_sort(items, p + 1, high)
+
+def quick_sort_top5(d):
+    items = list(d.items())
+    quick_sort(items)
+    return [key for key, _ in items[:5]]
+
+
