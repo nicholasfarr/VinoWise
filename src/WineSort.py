7fff5c4ffc1ac36f4f2fcb29b44e5ec7d6d77e81
sheff2 <shaheff01@gmail.com>
2025-04-19 18:38:03 -0400

    changed sorting to sort based on items

--- src/WineSort.py
+++ src/WineSort.py
@@ -1,13 +1,16 @@
 from sorts import *
 
-def findRecommendationsMerge(node, amount: int):        
-    sorted_items = merge_sort(node.neighbors)
+def findRecommendationsMerge(node, amount: int):    
+        
+    sorted_items = merge_sort(list(node.neighbors.items()))
     return [key for key, _ in sorted_items[:amount]]
 
 def findRecommendationsQuick(node, amount: int):
-    sorted_items = quick_sort(node.neighbors)
-    return [key for key, _ in sorted_items[:amount]]    
+    sorted_items = quick_sort(list(node.neighbors.items()))
+    return [key for key, _ in sorted_items[:amount]]
+
+#probably should have a function to find the best wine for a user based on their preferences
+#also have a way for user to test between quick sort and merge sort and return the time it takes
 
 
-def SortWineScore():
-    return 10
+# def findBestWine(wine_nodes, user_preferences):
