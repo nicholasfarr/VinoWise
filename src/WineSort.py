906e26238d1a7b727929480ff82969164efdf03c
LandonB5 <landon.b5@icloud.com>
2025-04-19 18:53:24 -0400

    measures the time of each sort

--- src/WineSort.py
+++ src/WineSort.py
@@ -1,13 +1,20 @@
 from sorts import *
+import time
 
-def findRecommendationsMerge(node, amount: int):    
-        
-    sorted_items = merge_sort(list(node.neighbors.items()))
-    return sorted_items[:amount]
+def findRecommendationsMerge(node, amount: int):
+    start = time.perf_counter()
+    sorted_items = merge_sort(node.neighbors)
+    recommendations = [key for key, _ in sorted_items[:amount]]
+    elapsed = time.perf_counter() - start
+    return recommendations, elapsed
+
 
 def findRecommendationsQuick(node, amount: int):
-    sorted_items = quick_sort(list(node.neighbors.items()))
-    return sorted_items[:amount]
+    start = time.perf_counter()
+    sorted_items = quick_sort(node.neighbors)
+    recommendations = [key for key, _ in sorted_items[:amount]]
+    elapsed = time.perf_counter() - start
+    return recommendations, elapsed
 
 #probably should have a function to find the best wine for a user based on their preferences
 #also have a way for user to test between quick sort and merge sort and return the time it takes
